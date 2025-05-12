c736c4d59eb463403a5c75ea61445878b135f838
Loc dev <locdybala11.it@gmail.com>
2025-05-12 10:34:35 +0700

    Add code

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,6 @@ use App\Http\Controllers\admin\RoleController;
 use App\Http\Controllers\admin\SettingController;
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', function () {
-        return view('welcome');
-    });
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
